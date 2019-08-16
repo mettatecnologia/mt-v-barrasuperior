4ef8dac51ef796d49113085a752d02c7ad78fc77
Joabe Machado <joabemachadodeabreu@gmail.com>
2019-08-16 14:32:51 -0300

    atualizado para o Vuetify 2.0

--- BarraSuperior.vue
+++ BarraSuperior.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
-        <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" :color="color" dark app fixed >
-            <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" :color="color" dark app fixed >
+            <v-toolbar-content style="width: 300px" class="ml-0 pl-3">
                 <v-toolbar-side-icon v-if="comMenu" @click.stop="abrir_menudrawer = !abrir_menudrawer"></v-toolbar-side-icon>
-                <span class="hidden-sm-and-down"><v-btn flat :href="href" class="v-btn-remove-fundo-before">{{titulo}}</v-btn></span>
-            </v-toolbar-title>
+                <span class="hidden-sm-and-down"><v-btn text :href="href" class="v-btn-remove-fundo-before">{{titulo}}</v-btn></span>
+            </v-toolbar-content>
 
             <slot></slot>
 
-        </v-toolbar>
+        </v-app-bar>
 
         <slot name="menu">
             <!-- <menulateral-principal v-if="comMenu" v-model="abrir_menudrawer" :itens="JSON.parse(menuItens)" ></menulateral-principal> -->
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
+let Vue = window.Vue
+
 Vue.component('jb-barrasuperior', require('./BarraSuperior.vue').default);
